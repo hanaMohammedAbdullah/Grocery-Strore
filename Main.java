1c7bc0e1e061b01605fdcda13d6cdc514f973edf
Hana Mohammad Abdullah <73670213+hanohirani@users.noreply.github.com>
2021-10-25 23:10:51 +0300

    new update

---
+++ Main.java
@@ -0,0 +1,14 @@
+import Collection.ManagerEmploDemo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        Scanner Input=new Scanner(System.in);
+        System.out.println("--------------------------------------------------");
+        System.out.println(" Grocery Shop System ");
+        ManagerEmploDemo.ManagerEmployee();
+    }
+}
